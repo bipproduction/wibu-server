ef6c9c2704ea80a92d0c7d0ccea93e040abc5592
bipproduction <bip.production.js@gmail.com>
2024-05-08 15:32:55 +0800

    auto push

--- src/app/admin/app/page.tsx
+++ src/app/admin/app/page.tsx
@@ -3,7 +3,7 @@ import app_config from "@/util/app_config";
 import { Stack, Title } from "@mantine/core";
 
 export default async function Page() {
-    const res = await fetch(app_config.host + '/api/app/list-app', { cache: "no-store", next: { revalidate: 0 } }).then(res => res.json())
+    const res = await fetch(app_config.host + '/api/app/list-app', { cache: "no-store"}).then(res => res.json())
     return <Stack p={"md"}>
         <Title order={3}>App</Title>
         <TableApp data={res} />
