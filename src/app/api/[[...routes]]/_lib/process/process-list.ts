ce4847e877f3a405e2c95bd1197045a69a18c19a
bipproduction <bip.production.js@gmail.com>
2025-03-13 19:55:10 +0800

    tamabahan

--- src/app/api/[[...routes]]/_lib/process/process-list.ts
+++ src/app/api/[[...routes]]/_lib/process/process-list.ts
@@ -1,16 +1,24 @@
 import { exec } from "child_process";
-import {promisify} from 'util'
+import { promisify } from "util";
 
-const X = promisify(exec)
+const X = promisify(exec);
 
 async function processList() {
-  const {stdout} = await X("pm2 jlist");
-  const data  = stdout.toString()
-  const match = data.match("\[[^{]*({.*})\]")
-  const newData = match ? match[1] : "[]"
-  return {
-    data: newData
-  };
+  const { stdout } = await X("pm2 jlist");
+  const data = stdout.toString();
+  const match = data.match("[[^{]*({.*})]");
+  const newData = match ? match[1] : "[]";
+
+  try {
+    const parsedData = JSON.parse(newData);
+    return {
+      data: parsedData,
+    };
+  } catch (error) {
+    return {
+      data: [],
+    };
+  }
 }
 
 export default processList;
