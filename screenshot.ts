53cdaaf3a40c72741ee88b5f7ae7cb77c3742238
bipproduction <bip.production.js@gmail.com>
2025-03-22 02:31:25 +0800

    tambah test

--- screenshot.ts
+++ screenshot.ts
@@ -1,27 +1,9 @@
-import puppeteer from 'puppeteer';
+import { chromium } from 'playwright';
 
 (async () => {
-  try {
-    // Launch a headless browser
-    console.log('Launching browser...');
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    // Navigate to the local server URL
-    const url = 'https://id.pngtree.com/free-png-vectors/gambar-kartun-lucu';
-    console.log(`Opening ${url}`);
-    await page.goto(url, { waitUntil: 'networkidle2' }); // Wait until the page is fully loaded
-
-    // Take a screenshot and save it as 'screenshot.png'
-    console.log('Taking screenshot...');
-    await page.screenshot({ path: 'screenshot.png', fullPage: true });
-
-    console.log('Screenshot saved as screenshot.png');
-
-    // Close the browser
-    console.log('Closing browser...');
-    await browser.close();
-  } catch (error) {
-    console.error('Error occurred:', error);
-  }
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto('https://id.pngtree.com/free-png-vectors/gambar-kartun-lucu');
+  await page.screenshot({ path: 'screenshot.png', fullPage: true });
+  await browser.close();
 })();
--- x.sh
+++ x.sh
@@ -1,2 +1,9 @@
-source .env
-curl -H "Authorization: Bearer $WIBU_GH_TOKEN" https://api.github.com/repos/bipproduction/wibu-server/actions/artifacts
+# source .env
+# curl -H "Authorization: Bearer $WIBU_GH_TOKEN" https://api.github.com/repos/bipproduction/wibu-server/actions/artifacts
+
+E=$(cat <<EOF
+console.log("Hello World");
+EOF
+)
+
+bun -e "$E"
