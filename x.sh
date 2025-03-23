1e9b80f586ffc78ceff117639829f867de1ae710
bipproduction <bip.production.js@gmail.com>
2025-03-23 13:39:22 +0800

    tambah test

--- x.sh
+++ x.sh
@@ -1,5 +1,20 @@
 source .env
-curl -L -o artifact.zip \
--H "Authorization: Bearer $WIBU_GH_TOKEN" \
-https://api.github.com/repos/bipproduction/wibu-server/actions/artifacts/2798186096/zip
+# Encode gambar ke base64
+BASE64_CONTENT=$(base64 red.jpg | tr -d '\n')
 
+RANDOM_NAME=$(date +%s%N | md5sum | head -c 10)
+
+# Upload ke repository
+RESULT=$(curl -X PUT \
+  -s \
+  -H "Authorization: token $WIBU_GH_TOKEN" \
+  -H "Accept: application/vnd.github.v3+json" \
+  -d '{
+    "message": "Upload image",
+    "content": "'"$BASE64_CONTENT"'",
+    "branch": "main"
+  }' \
+  https://api.github.com/repos/bipproduction/assets/contents/images/${RANDOM_NAME}.jpg | jq -r '.content.download_url'
+)
+
+echo $RESULT
