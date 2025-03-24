b6e2088167f592b47b6434015e05dab18ea812a2
bipproduction <bip.production.js@gmail.com>
2025-03-24 10:33:42 +0800

    tambah test

--- x.sh
+++ x.sh
@@ -1,20 +1,22 @@
-source .env
-# Encode gambar ke base64
-BASE64_CONTENT=$(base64 red.jpg | tr -d '\n')
+# source .env
+# # Encode gambar ke base64
+# BASE64_CONTENT=$(base64 red.jpg | tr -d '\n')
 
-RANDOM_NAME=$(date +%s%N | md5sum | head -c 10)
+# RANDOM_NAME=$(date +%s%N | md5sum | head -c 10)
 
-# Upload ke repository
-RESULT=$(curl -X PUT \
-  -s \
-  -H "Authorization: token $WIBU_GH_TOKEN" \
-  -H "Accept: application/vnd.github.v3+json" \
-  -d '{
-    "message": "Upload image",
-    "content": "'"$BASE64_CONTENT"'",
-    "branch": "main"
-  }' \
-  https://api.github.com/repos/bipproduction/assets/contents/images/${RANDOM_NAME}.jpg | jq -r '.content.download_url'
-)
+# # Upload ke repository
+# RESULT=$(curl -X PUT \
+#   -s \
+#   -H "Authorization: token $WIBU_GH_TOKEN" \
+#   -H "Accept: application/vnd.github.v3+json" \
+#   -d '{
+#     "message": "Upload image",
+#     "content": "'"$BASE64_CONTENT"'",
+#     "branch": "main"
+#   }' \
+#   https://api.github.com/repos/bipproduction/assets/contents/images/${RANDOM_NAME}.jpg | jq -r '.content.download_url'
+# )
 
-echo $RESULT
+# echo $RESULT
+
+echo  "$APA" == "false" 
