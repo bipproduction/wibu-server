6d141abc8599d51abce459d766b05f0a4170f375
bipproduction <bip.production.js@gmail.com>
2025-03-26 14:13:50 +0800

    tambahan

--- src/components/process/ProcessList.tsx
+++ src/components/process/ProcessList.tsx
@@ -1,40 +1,41 @@
 import processState from "@/state/process";
-import { Stack, Title, Table, Text } from "@mantine/core";
+import { Stack, Title, Table, Text, Tooltip } from "@mantine/core";
 import { useShallowEffect } from "@mantine/hooks";
 import _ from "lodash";
 import { useSnapshot } from "valtio";
 
 function ProcessList() {
-    const process = useSnapshot(processState);
-    useShallowEffect(() => {
-      process.load();
-    }, []);
-    return (
-      <Stack bg={"dark.9"} p={"md"}>
-        <Title order={3}>Process List</Title>
-        <div
-          style={{
-            width: "100%",
-            overflow: "scroll",
-          }}
-        >
-          <Table>
-            <Table.Thead>
-              <Table.Tr bg={"gray.9"}>
-                {_.keys(process.list[0]).map((key) => (
-                  <Table.Th key={key}>
-                    <Text>{key}</Text>
-                  </Table.Th>
-                ))}
-              </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody>
-              {process.list.map((item, index) => (
-                <Table.Tr key={index}>
-                  {_.keys(item).map((key) => (
-                    <Table.Td key={key}>
+  const process = useSnapshot(processState);
+  useShallowEffect(() => {
+    process.load();
+  }, []);
+  return (
+    <Stack bg={"dark.9"} p={"md"}>
+      <Title order={3}>Process List</Title>
+      <div
+        style={{
+          width: "100%",
+          overflow: "scroll",
+        }}
+      >
+        <Table>
+          <Table.Thead>
+            <Table.Tr bg={"gray.9"}>
+              {_.keys(process.list[0]).map((key) => (
+                <Table.Th key={key}>
+                  <Text>{key}</Text>
+                </Table.Th>
+              ))}
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {process.list.map((item, index) => (
+              <Table.Tr key={index}>
+                {_.keys(item).map((key) => (
+                  <Table.Td key={key} maw={230}>
+                    <Tooltip label={item[key]}>
                       <Text
-                        miw={key === "name" ? 200 : 100}
+                        lineClamp={1}
                         c={
                           key === "status" && item[key] === "online"
                             ? "green"
@@ -43,15 +44,16 @@ function ProcessList() {
                       >
                         {item[key] ?? "-"}
                       </Text>
-                    </Table.Td>
-                  ))}
-                </Table.Tr>
-              ))}
-            </Table.Tbody>
-          </Table>
-        </div>
-      </Stack>
-    );
-  }
+                    </Tooltip>
+                  </Table.Td>
+                ))}
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </Table>
+      </div>
+    </Stack>
+  );
+}
 
-  export default ProcessList;
+export default ProcessList;
