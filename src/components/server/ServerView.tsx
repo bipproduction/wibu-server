c650dc4f8fa3a94758ea410a7336e8a0d35cc2df
bipproduction <bip.production.js@gmail.com>
2025-03-29 09:14:34 +0800

    tambahan

--- src/components/server/ServerView.tsx
+++ src/components/server/ServerView.tsx
@@ -1,16 +1,151 @@
-import { Stack } from "@mantine/core";
-import ServerViewMuku from "./ServerViewMuku";
-import ServerViewWibuDev from "./ServerViewWibudev";
-import { Title } from "@mantine/core";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import serverState from "@/state/server";
+import {
+  ActionIcon,
+  Button,
+  Card,
+  CloseButton,
+  Flex,
+  Group,
+  Indicator,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { useShallowEffect } from "@mantine/hooks";
+import { IconEdit, IconTrash } from "@tabler/icons-react";
+import { useSnapshot } from "valtio";
+import { useProxy } from "valtio/utils";
 
 function ServerView() {
-    return (
-      <Stack>
-        <Title order={3}>Server</Title>
-        <ServerViewMuku />
-        <ServerViewWibuDev />
-      </Stack>
-    );
+  const server = useSnapshot(serverState);
+
+  useShallowEffect(() => {
+    serverState.load();
+  }, []);
+  return (
+    <Stack p={"md"}>
+      <View name="muku" data={server.muku as any[]} />
+      <View name="wibuDev" data={server.wibudev as any[]} />
+    </Stack>
+  );
+}
+
+function View({ name, data }: { name: string; data: any[] | null }) {
+  const server = useProxy(serverState);
+  if (!data) return <Text>loading ...</Text>;
+
+  if (server.event?.name === name && server.event.action === "add") {
+    return <ServerAdd />;
   }
 
-  export default ServerView;
+  if (server.event?.name === name && server.event.action === "update" && server.updateData) {
+    return <ServerUpdate />;
+  }
+
+  return (
+    <Stack>
+      <Stack bg={"dark.9"} p={"xs"}>
+        <Flex>
+          <Title order={4}>{name}</Title>
+        </Flex>
+        <Button.Group>
+          <Button
+            onClick={() => (server.event = { name, action: "add" })}
+            variant="light"
+            size="compact-xs"
+          >
+            Add
+          </Button>
+          <Button
+            onClick={() => (server.event = { name, action: "remove" })}
+            variant="light"
+            size="compact-xs"
+          >
+            Remove
+          </Button>
+          <Button
+            onClick={() => (server.event = { name, action: "update" })}
+            variant="light"
+            size="compact-xs"
+          >
+            Update
+          </Button>
+        </Button.Group>
+      </Stack>
+      <Stack>
+        {data.map((item, index) => (
+          <Flex key={index} gap={"md"}>
+            {server.event?.name === name && server.event?.action === "remove" && (
+              <Indicator label={server.deleteCount}>
+                <ActionIcon onClick={() => server.onRemove({domainId: name, id: item.id})} c={"red"} variant="transparent">
+                 <IconTrash />
+              </ActionIcon>
+              </Indicator>
+            )}
+            {server.event?.name === name && server.event?.action === "update" && (
+              <ActionIcon
+                onClick={() => (server.updateData = item)}
+                variant="transparent"
+              >
+                <IconEdit />
+              </ActionIcon>
+            )}
+            <Text w={20}>{index + 1}</Text>
+            <Text w={220}>{item.name}</Text>
+            <Text>{item.ports.join(",")}</Text>
+          </Flex>
+        ))}
+      </Stack>
+    </Stack>
+  );
+}
+function ServerUpdate() {
+  const server = useProxy(serverState);
+  return (
+    <Group>
+      <Card bg={"dark.9"} miw={300}>
+        <Stack>
+          <Flex align={"center"} gap={"md"}>
+            <CloseButton
+              onClick={() => {
+                server.event = { name: "", action: "update" }
+                server.updateData = null;
+              }}
+            />
+            <Text>Server Update</Text>
+          </Flex>
+          <Text>{server.event?.name}</Text>
+          <TextInput value={server.updateData?.name} label="Name" onChange={(e) => (server.updateData = { ...server.updateData, name: e.target.value })} />
+          <TextInput value={server.updateData?.ports?.join(", ")} label="Port" onChange={(e) => (server.updateData = { ...server.updateData, ports: e.target.value.split(", ").map((p) => Number(p)) })} />
+          <Button onClick={() => server.onUpdate(server.updateData as any)} variant="light">Submit</Button>
+        </Stack>
+      </Card>
+    </Group>
+  );
+}
+
+function ServerAdd() {
+  const server = useProxy(serverState);
+  return (
+    <Group>
+      <Card bg={"dark.9"} miw={300}>
+        <Stack>
+          <Flex align={"center"} gap={"md"}>
+            <CloseButton
+              onClick={() => (server.event = { name: "", action: "add" })}
+            />
+            <Text>Server Add</Text>
+          </Flex>
+          <Text>{server.event?.name}</Text>
+          <TextInput label="Name" />
+          <TextInput label="Port" />
+          <Button variant="light">Submit</Button>
+        </Stack>
+      </Card>
+    </Group>
+  );
+}
+
+export default ServerView;
--- src/state/server.ts
+++ src/state/server.ts
@@ -1,47 +1,84 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import ApiFetch from "@/lib/api-fetch";
+import toast from "react-simple-toasts";
 import { proxy } from "valtio";
 
-type ServerState = {
-  muku: {
-    json: any[];
-    table: string;
-    load: () => Promise<void>;
-  };
-  wibudev: {
-    json: any[];
-    table: string;
-    load: () => Promise<void>;
-  };
-};
-
-const serverState = proxy<ServerState>({
-  muku: {
-    json: [],
-    table: "",
-    async load() {
-      const { data } = await ApiFetch.api.server.config.get();
-      const ls = data?.data.muku.subdomains as any[];
-      if (ls) {
-        serverState.muku.json = ls;
-      }
-      const dataTabele = await ApiFetch.api.server["table-muku"].get();
-      serverState.muku.table = dataTabele.data?.data as string;
-    },
+const serverState = proxy({
+  muku: null as any[] | null,
+  wibudev: null as any | null,
+  deleteCount: 0,
+  updateData: null as {
+    id?: string | undefined;
+    domainId?: string | undefined;
+    name?: string | undefined;
+    ports?: number[] | undefined;
+  } | null,
+  async load() {
+    const { data } = await ApiFetch.api.server.config.get();
+    const lsMuku = data?.data.muku.subdomains as any[];
+    if (lsMuku) {
+      this.muku = lsMuku;
+    }
+    const lsWibuDev = data?.data.wibuDev.subdomains as any[];
+    if (lsWibuDev) {
+      this.wibudev = lsWibuDev;
+    }
   },
-  wibudev: {
-    json: [],
-    table: "",
-    async load() {
-      const { data } = await ApiFetch.api.server.config.get();
-      const ls = data?.data.wibuDev.subdomains as any[];
-      if (ls) {
-        serverState.wibudev.json = ls;
+  event: null as null | {
+    name: string;
+    action: "add" | "remove" | "update";
+  },
+  async onUpdate(params: {
+    domainId: string;
+    id: string;
+    name: string;
+    ports: number[];
+  }) {
+    if (params.domainId === "muku") {
+      const newData = this.muku?.map((item: any) => {
+        if (item.id === params.id) {
+          return params;
+        }
+        return item;
+      });
+      this.muku = newData as any[];
+      this.updateData = null;
+      this.event = null;
+    }
+
+    if (params.domainId === "wibuDev") {
+      const newData = this.wibudev?.map((item: any) => {
+        if (item.id === params.id) {
+          return params;
+        }
+        return item;
+      });
+      this.wibudev = newData as any[];
+    }
+  },
+  async onRemove({ domainId, id }: { domainId: string; id: string }) {
+    this.deleteCount++;
+    toast(`${this.deleteCount} / 5 to delete`);
+    if (this.deleteCount === 5) {
+      if (domainId === "muku") {
+        const newData = this.muku?.filter((item: any) => item.id !== id);
+        this.muku = newData as any[];
       }
 
-      const dataTabele = await ApiFetch.api.server["table-wibudev"].get();
-      serverState.wibudev.table = dataTabele.data?.data as string;
-    },
+      if (domainId === "wibuDev") {
+        const newData = this.wibudev?.filter((item: any) => item.id !== id);
+        this.wibudev = newData as any[];
+      }
+
+      this.event = null;
+      this.deleteCount = 0;
+      toast(`${id} Deleted!`);
+      return;
+    }
+
+    setTimeout(() => {
+      this.deleteCount = 0;
+    }, 5000);
   },
 });
 
