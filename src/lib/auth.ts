c82723f0e6a5a71d927a8b7fc07b271933864a45
bipproduction <bip.production.js@gmail.com>
2025-04-05 05:23:34 +0800

    tambahan

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,21 +1,25 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
- 
+
 const prisma = new PrismaClient();
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
-    }),
-    emailAndPassword: {  
-        enabled: true
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  redirectTo: "/profile",
+
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
-    redirectTo: "/profile",
-    
-    socialProviders: { 
-        github: { 
-           clientId: process.env.GITHUB_CLIENT_ID as string, 
-           clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
-        }, 
-    }, 
+  },
+  trustedOrigins: [
+    process.env.BASE_URL as string,
+    "http://85.31.224.193:3006"
+  ],
 });
